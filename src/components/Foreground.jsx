29582b6d853a94ad0bbf16ed08eda3b1afb35ce1
bipinkatuwal567 <bipinkatuwal098@gmail.com>
2024-03-21 16:25:10 +0545

    completed project with use of framer motion

--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,11 +1,51 @@
-import React from "react";
+import React, { useRef } from "react";
 import Card from "../ui/Card";
 
 function Foreground() {
-    const data = [{description: "Lorem ipsum dolor sit, amet consectetur adipisicing.", fileSize: ".45mb", closeOrDownload, tagDetails}]
-  return <div className="fixed top-0 left-0 w-full h-full z-20">
-    <Card />
-  </div>;
+  const ref = useRef(null);
+
+  const data = [
+    {
+      description: "Lorem ipsum dolor sit, amet consectetur adipisicing.",
+      fileSize: ".45mb",
+      close: true,
+      tag: {
+        tagTitle: "Download Now",
+        isOpen: true,
+        tagColor: "green",
+      },
+    },
+    {
+      description: "Lorem ipsum dolor sit, amet consectetur adipisicing.",
+      fileSize: ".89mb",
+      close: true,
+      tag: {
+        tagTitle: "Download Now",
+        isOpen: false,
+        tagColor: "blue",
+      },
+    },
+    {
+      description: "Lorem ipsum dolor sit, amet consectetur adipisicing.",
+      fileSize: ".12mb",
+      close: false,
+      tag: {
+        tagTitle: "Download Now",
+        isOpen: true,
+        tagColor: "blue",
+      },
+    },
+  ];
+  return (
+    <div
+      ref={ref}
+      className="fixed flex p-5 gap-10 flex-wrap top-0 left-0 w-full h-full z-20"
+    >
+      {data.map((item, index) => (
+        <Card data={item} key={index} reference={ref} />
+      ))}
+    </div>
+  );
 }
 
 export default Foreground;
--- src/ui/Card.jsx
+++ src/ui/Card.jsx
@@ -1,24 +1,46 @@
 import React from "react";
 import { FaRegFileAlt } from "react-icons/fa";
 import { MdDownloadForOffline } from "react-icons/md";
+import { IoClose } from "react-icons/io5";
+import { motion } from "framer-motion";
 
-function Card() {
+function Card({ data, reference }) {
   return (
-    <div className="relative w-60 h-72 rounded-[45px] overflow-hidden bg-zinc-900/90 text-zinc-200 p-8">
+    <motion.div
+      drag
+      dragConstraints={reference}
+      whileDrag={{ scale: 1.1 }}
+      dragElastic={0.1}
+      dragTransition={{ bounceStiffness: 600, bounceDamping: 10 }}
+      className="relative flex-shrink-0 w-60 h-72 rounded-[45px] overflow-hidden bg-zinc-900/90 text-zinc-200 p-8"
+    >
       <FaRegFileAlt />
-      <p className="mt-5 text-sm leading-2">Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
+      <p className="mt-8 text-sm leading-2">{data.description}</p>
+
       <div className="absolute left-0 bottom-0 w-full">
-        <div className="w-full px-8 flex justify-between items-center mb-4">
-           <p className="text-sm">.95mb</p> 
-           <span>
-            <MdDownloadForOffline size="1.5rem" />
-           </span>
+        <div className="w-full px-8 flex justify-between items-center mb-5">
+          <p className="text-sm">{data.fileSize}</p>
+          <span className=" cursor-pointer">
+            {data.close ? (
+              <IoClose size="1.2rem" />
+            ) : (
+              <MdDownloadForOffline size="1.3rem" />
+            )}
+          </span>
         </div>
-        <div className="py-3.5 bg-green-500">
-            <p className="text-center text-sm">Download Now</p>
-        </div>
+        {data.tag.isOpen && (
+          <div
+            className={`py-3.5 ${
+              data.tag.tagColor === "blue" ? "bg-blue-600" : "bg-green-600"
+            }`}
+          >
+            <p className="text-center text-sm cursor-pointer">
+              {data.tag.tagTitle}
+            </p>
+          </div>
+        )}
       </div>
-    </div>
+    </motion.div>
   );
 }
 
